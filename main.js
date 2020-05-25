6fe7f545c0197845687c6b84106a4414dd8ec84f
Zargy <zargyyoyo@gmail.com>
2020-05-25 18:18:31 -0500

    Finding a style.

---
+++ main.js
@@ -0,0 +1,10 @@
+
+/*var quotes = [
+	"Mistakes were made...",
+	"Born of god and meme...",
+	
+	"TEST3",
+];
+
+var idx = Math.floor(Math.random() * quotes.length)
+document.getElementById('quote').innerHTML = quotes[idx];*/
